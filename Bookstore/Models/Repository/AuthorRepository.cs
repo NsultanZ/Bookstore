9ae2201bb1024c714c833a14efe99a3b6456d4a1
sultan <n.sultan@obeikan.com.sa>
2021-09-21 10:31:16 +0300

    bookstore1

---
+++ Bookstore/Models/Repository/AuthorRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Models.Repository
+{
+    public class AuthorRepository : IBookstoreRepository<Author>
+    {
+        IList<Author> authors;
+
+        public AuthorRepository()
+        {
+            authors = new List<Author>()
+            {
+
+                new Author { Id=1, FullName ="Nsultan saad"},
+                new Author { Id=2, FullName ="Nsultan Zsaad"},
+                new Author { Id=3, FullName ="Nsultan Hsaad"}
+            };
+        }
+        public void Add(Author entity)
+        {
+            authors.Add(entity);
+        }
+
+        public void Delete(int id)
+        {
+            var author = Find(id);
+            authors.Remove(author);
+        }
+
+        public Author Find(int id)
+        {
+            var author = authors.SingleOrDefault(a => a.Id == id);
+            return author;
+        }
+
+        public IList<Author> List()
+        {
+            return authors;
+        }
+
+        public void Update(int id, Author newAuthor)
+        {
+            var author = Find(id);
+            author.FullName = newAuthor.FullName;
+            
+        }
+    }
+}
---
+++ Bookstore/Models/Repository/BookRepository.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Models.Repository
+{
+    public class BookRepository : IBookstoreRepository<Book>
+    {
+        List<Book> books;
+        public BookRepository()
+        {
+            books = new List<Book>()
+             {
+                 new Book
+                 {
+                     Id=1, Titel="c# programing", Description="no description"
+                 },
+
+                 new Book
+                 {
+                     Id=2, Titel="java programing", Description="nothing"
+                 },
+
+                 new Book
+                 {
+                     Id=3, Titel="python programing", Description="no data"
+                 },
+
+             };
+        }
+        
+        public void Add(Book entity)
+        {
+            books.Add(entity);
+        }
+
+        public void Delete(int id)
+        {
+            var book = Find(id);
+
+            books.Remove(book);
+        }
+
+        public Book Find(int id)
+        {
+            var book = books.SingleOrDefault(b => b.Id == id);
+            return book;
+        }
+
+        public IList<Book> List()
+        {
+            return books;
+        }
+
+        public void Update(int id,Book NewBook)
+        {
+            var book = Find(id);
+            book.Titel = NewBook.Titel;
+            book.Description = NewBook.Description;
+            book.Author = NewBook.Author;
+        }
+    }
+}
